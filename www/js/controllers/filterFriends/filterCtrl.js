f04dff5e81df02be20ffb88bafc43478e1335d26
shachar romi <shachr0@gmail.com>
2016-06-03 17:45:17 +0300

    WS-11.0 <edanshnitzer@EDANSHNITZE259C Overwrite local to https://github.com/NoamRom89/Honeybee---School.git

--- www/js/controllers/filterFriends/filterCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/** 
-  * controller  - filter
-*/
-app.controller('FilterCtrl', function($scope, $rootScope, $http, userFactory, APP_REGEX, $localstorage, $state,$ionicPopup){
-	console.log('I am a filterCtrl');
-});
--- www/js/controllers/notificationCtrl.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-/** 
-  * controller  - filter
-*/
-app.controller('NotificationCtrl', function($scope, $rootScope, $http, userFactory, APP_REGEX, $localstorage, $state,$ionicPopup){
-	console.log('I am a notificationCtrl');
-});
